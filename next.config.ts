e8c6de316537a7bdff51b09f677a11658e8e10ed
kono-meowmeow <aya.ayatsuki@gmail.com>
2026-01-04 21:19:56 +0900

    COOPヘッダーの追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              // GoogleOauthはポップアップを使うため、COOPヘッダーを追加してクロスオリジンのポップアップを許可する必要がある
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin-allow-popups',
+            },
+          ],
+        }
+      ]
+  },
 };
 
 export default nextConfig;
