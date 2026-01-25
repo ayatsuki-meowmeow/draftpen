63a93e9b7fdaf153ec1b29d577e962b70fe3e453
kono-meowmeow <aya.ayatsuki@gmail.com>
2026-01-25 19:47:43 +0900

    ログイン画面への動線

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <>
-      <div>
+      <div className="flex flex-col text-center p-4">
         <h1 className="text-2xl font-bold mb-4">Welcome to DraftPen!</h1>
+        <div className="text-gray-600">
+          <p>ログインは<a href="/admin/auth" className="text-blue-500 hover:underline">こちら</a>から</p>
+        </div>
       </div>
     </>
   );
