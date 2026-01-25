cd523cdfb15741ab3ced62ae021ce5d1e67038c3
kono-meowmeow <aya.ayatsuki@gmail.com>
2026-01-25 23:23:21 +0900

    管理画面のヘッダー

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,9 +3,6 @@ function App() {
     <>
       <div className="flex flex-col text-center p-4">
         <h1 className="text-2xl font-bold mb-4">Welcome to DraftPen!</h1>
-        <div className="text-gray-600">
-          <p>ログインは<a href="/admin/auth" className="text-blue-500 hover:underline">こちら</a>から</p>
-        </div>
       </div>
     </>
   );
