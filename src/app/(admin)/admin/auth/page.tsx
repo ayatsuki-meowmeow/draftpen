149e2f9361f069829e8cef47d86d2ed038805dad
ayatsuki-meowmeow <aya.ayatsuki@gmail.com>
2026-03-21 22:12:17 +0900

    mock articles

--- src/app/(admin)/admin/auth/page.tsx
+++ src/app/(admin)/admin/auth/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { db } from "@/lib/db";
 import UserInfo from "@/components/admin/auth/userInfo";
 import Login from "@/components/admin/auth/login";
