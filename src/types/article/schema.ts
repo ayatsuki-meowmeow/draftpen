94d41cf44b2f5e9972f5d14df6723eb88231058e
ayatsuki-meowmeow <aya.ayatsuki@gmail.com>
2026-03-21 21:40:16 +0900

    記事の型定義

---
+++ src/types/article/schema.ts
@@ -0,0 +1,8 @@
+import { AppSchema } from "@/instant.schema";
+import { InstaQLEntity } from "@instantdb/react";
+
+export type Article = Omit<InstaQLEntity<AppSchema, "articles">, "publishedAt" | "createdAt" | "updatedAt"> & {
+  publishedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
